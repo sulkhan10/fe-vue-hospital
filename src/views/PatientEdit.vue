8ddbf91d37e5f57bd1eedd751f60460c4f03642f
sulkhan10 <sulkhangalangsakti@gmail.com>
2023-09-17 04:19:21 +0700

    fix : select option sex field

--- src/views/PatientEdit.vue
+++ src/views/PatientEdit.vue
@@ -140,8 +140,9 @@ onMounted(() => {
               name="sex"
               class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
             >
-              <option value="male">Male</option>
-              <option value="female">Female</option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Other">Other</option>
             </select>
           </div>
           <div v-if="errors.sex">
